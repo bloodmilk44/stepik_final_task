dbdd402a8dacdc231a368721f7f313847e430b0b
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-03-09 15:18:04 +0400

    Update login_page.py
    Добавлен метод для генерации почты для проверки регистрации

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,30 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
+
+    def register_new_user(self, email, password):
+        mail = self.browser.find_element(*LoginPageLocators.EMAIL)
+        mail.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.PASSWORD)
+        password_input.send_keys(password)
+        password_confirm = self.browser.find_element(*LoginPageLocators.PASSWORD_CONFIRM)
+        password_confirm.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REG_BUTTON)
+        reg_button.click()
+
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "'login' not in current url"
-
+        assert "login" in self.browser.current_url, "Is not 'login' in URL"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login Button is not presented"
-
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "Register Button is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
