44626a5372d4410e6551987b33a842f7723ee04c
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-03-09 15:17:08 +0400

    Update locators.py
    Обновлены локаторы (добавлены локаторы для страницы с продуктом)

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,31 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group > a")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators():
+    BASKET_INNER = (By.CSS_SELECTOR, "#content_inner > p")
+    BASKET_TITLE = (By.CSS_SELECTOR, ".alertinner > p")
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
 
 
 class LoginPageLocators():
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
-    LOGIN_BUTTON = (By.NAME, "login_submit")
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BUTTON = (By.CSS_SELECTOR, ".register_form .btn-primary")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    ADD_MESSAGE = (By.CSS_SELECTOR, ".alertinner > strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
