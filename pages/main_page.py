897b32b5dba3a07e5cf720c1c3eb57887a0c24cb
bloodmilk44 <59761531+bloodmilk44@users.noreply.github.com>
2020-03-09 15:19:18 +0400

    Update main_page.py
    Изменены методы главной страницы

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,21 +1,8 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
-from .login_page import LoginPage
+from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
-    def test_guest_can_go_to_login_page(browser):
-        link = "http://selenium1py.pythonanywhere.com"
-        page = MainPage(browser, link)
-        page.open()
-        login_page = page.go_to_login_page()
-        login_page.should_be_login_page()
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
